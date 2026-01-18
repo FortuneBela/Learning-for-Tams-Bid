ed5e8c277f61ea3546cce8ba48b0aaf358177a18
FortuneBela <zachknifex@gmail.com>
2026-01-18 07:51:35 -0500

    Resolution and UI update
    Changed default resolution, nicer options menu, and slidebars that dont work

--- Scripts/options_menu.gd
+++ Scripts/options_menu.gd
@@ -1,16 +1,15 @@
 extends Node
 
-@onready var window : Panel = get_node("Options Panel")
+@onready var options_window : Panel = get_node("Options Panel")
 
 func _ready():
-	window.visible = false
-	
+	options_window.visible = false
 
 
 
 func _on_options_button_pressed():
-	window.visible = !window.visible
+	options_window.visible = !options_window.visible
 
 
 func _on_back_button_pressed():
-	window.visible = !window.visible
+	options_window.visible = !options_window.visible
